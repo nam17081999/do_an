74ed497fc301c84a433c58ccb456ade4a25d9e87
nam-tuoc <nam17081999@gmail.com>
2021-12-18 14:53:01 +0700

    them sv

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+var ip = require("ip");
+const path = require("path");
+const bodyParser = require("body-parser");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var plate = { message: "2222" };
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  io.emit("connected", { msg: "connected" });
+  io.emit("vitriread", plate);
+  socket.on("vitri", (msg) => {
+    socket.broadcast.emit("vitriread", msg);
+    return (plate = msg);
+  });
+  socket.on("arduino", function (data) {
+    io.sockets.emit('atime', { time: new Date().toJSON() });
+  });
+});
+http.listen(process.env.PORT || 3001, () => {});
